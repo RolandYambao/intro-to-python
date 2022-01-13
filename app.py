82dcabdceff3e909a4bf8394b3ca10260eacbe42
RolandYambao <rolandyambao@gmail.com>
2022-01-13 10:19:07 -0800

    Updated

--- app.py
+++ app.py
@@ -12,12 +12,39 @@ name = "Johnny"
 
 nothing = None
 
-is_working = False
+is_working = True
 car_off = False
 
-if is_working:
+if nothing:
     print('This is true')
     num = 7
-    print(7)
+    print('car is off')
+elif car_off:
+    print('this is the second condition')
+    print('this will run if car_off is True')
+elif is_working:
+    print('This is working')
 else:
     print('This is not true...')
+
+# this is another conditional
+if is_working:
+    print('This is working also')
+
+print(15 / 6)
+print(15 // 6)
+
+print("ace of spades".split(" "))
+# => ["ace", "of", "spades"]
+
+print("ace-of-spades".split("-"))
+# => ["ace", "of", "spades"]
+
+print("ace.of.spades".split("."))
+# => ["ace", "of", "spades"]
+
+my_scare = "boo"
+my_scare.upper()
+
+# conditional --> this first item that represents
+# True, it runs that indented [block]
