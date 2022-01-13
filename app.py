b804c266169c549c55dff0a59a5951f7a24c485b
RolandYambao <rolandyambao@gmail.com>
2022-01-13 09:49:51 -0800

    Updated

---
+++ app.py
@@ -0,0 +1,23 @@
+# this is a comment
+
+from pickle import TRUE
+
+
+def add(num, num2):
+    '''
+    this is suppose to add 2 numbers
+    '''
+
+name = "Johnny" 
+
+nothing = None
+
+is_working = False
+car_off = False
+
+if is_working:
+    print('This is true')
+    num = 7
+    print(7)
+else:
+    print('This is not true...')
