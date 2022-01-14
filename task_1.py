7901aba61f90d193217201bb20630d036702faf5
RolandYambao <rolandyambao@gmail.com>
2022-01-13 17:01:50 -0800

    Updated

---
+++ task_1.py
@@ -0,0 +1,37 @@
+print(4 + 4 / 2) # 6.0
+print((2 + -7) * 3) # -15
+print(101 % 10) # 1
+print(12 - 4 % 3) # 11
+print(True and False) # False
+print(True and not (False or False)) # True
+print(not True and not(False or False)) # False
+print('cat' + 'dog') #catdog
+
+number = 2
+food = "pizza"
+combine = f"{number}{food}"
+print(combine) # 2pizza
+
+# print(2.5 * 'fish') # Error
+print(5 >= 11) # False
+print(5 == 5.0 ) # True
+print(7 != 7.1) # True
+print(5 + 5 > 8) # True
+print(6 + 6 != 12) # False
+print(2 > 1 or False) # True
+print('true' == True) # False
+print(10 % 2 == 0) # True
+print(21 % 2 == 0) # False
+print(21 % 2 != 0) # True
+print(21 % 2 == 1) # True
+print(12 % 3 == 0) # True
+print(9 % 3 == 0) # True
+print(14 % 3 == 0) # False
+print('new york'[0]) # n
+print('new york'[1]) # e
+print('san francisco'[2 * 3]) # a
+print('engineering'[3].upper()) # I
+# print('engineering'.index('G')) # Error
+print('engineering'.index('neer')) # 4
+print('engineering'.index('r') > -1) # True
+# print('science'.index('x') == -1) # Error
